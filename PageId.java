a9daf94581f1cc1291d0b05a696ea0e7ae139ff3
maanlzz <manon_horan@hotmail.com>
2024-09-26 15:23:09 +0200

    Classe PageId

---
+++ PageId.java
@@ -0,0 +1,8 @@
+public class PageId {
+    private int FileIdx;
+    private int PageIdx;
+    public PageId(int FileIdx, int PageIdx) {
+        this.FileIdx = FileIdx;
+        this.PageIdx = PageIdx;
+    }
+}
