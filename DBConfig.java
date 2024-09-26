8ea3b98bbe28752d5d3ec6a5d7a1531dadc11abe
caijos83 <caijos83@gmail.com>
2024-09-26 17:44:01 +0200

    Update DBConfig.java

--- DBConfig.java
+++ DBConfig.java
@@ -1,17 +1,55 @@
-
 package projet_SGBD;
 import java.io.*;
-//test 
+import java.util.Scanner;
+import java.lang.Math;
 public class DBConfig {
-	public String dbpath;
+	String dbpath;
+	int pagesize;
+	long dm_maxfilesize;
+	
 	DBConfig(String dbpath){
 		this.dbpath=dbpath;
+	}
+	DBConfig(String dbpath,int pagesize,long dm_maxfilesize){
+		this.dbpath=dbpath;
+		this.pagesize=pagesize;
+		this.dm_maxfilesize=dm_maxfilesize;
 	}
 	public String getDbath() {
 		return dbpath;
 	}
-public static DBConfig LoadDBConfig(String fichier_config) throws IOException{
-	StringBuffer sb = new StringBuffer();
-		return fichier_config;
+	public int getPageSize() {
+		return pagesize;
+	}
+	public long getDm_maxfilesize() {
+		return dm_maxfilesize;
+	}
+
+	public static DBConfig LoadDBConfig(File fichier_config) throws FileNotFoundException{
+		DBConfig dbConfig=null;
+		Scanner myReader = new Scanner(fichier_config);
+		int pagesize=0;
+		int ligne=0;
+		while(myReader.hasNextLine()) {
+			String data = myReader.nextLine();
+			int pagetotal=(int) Math.ceil((double)fichier_config.length()/4096);
+			System.out.println("page total"+pagetotal);
+			ligne+=data.length();
+			System.out.println("ligne"+ligne);
+			
+			pagesize=(int)Math.ceil((double)ligne/4096);
+			System.out.println("*******page"+pagesize);
+			
+			dbConfig=new DBConfig(data,pagesize,fichier_config.length());
+			
+			dbConfig.afficheinfo();
+			
+		}
+		myReader.close();
+		return dbConfig;
+		}
+	
+	public void afficheinfo() {
+		System.out.println("contenu:"+dbpath+"\n"+"page:"+pagesize+"\n"+"taille:"+dm_maxfilesize);
 	}
 }
